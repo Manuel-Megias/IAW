d0dd5f79cf72e19ffcf3f81ab8d50591094167fb
ManuM <manu_laika@hotmail.com>
2019-10-03 10:16:57 +0200

    primer commit

---
+++ php/constantes_Manuel.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Constantes</title>
+</head>
+<body>
+    <?php
+    define ("COLOR_ROJO"," #FF0000");
+    define("EMPRESA","Zend.S.A.");
+    define("TEMPERATURA_EBULLICION", "100 Grados");
+    define("CAPITAL_ALBANIA", "Tirana");
+    define("CAPITAL_MACEDONIA" , "Skopje");
+    echo COLOR_ROJO;
+    echo EMPRESA;
+    echo TEMPERATURA_EBULLICION;
+    echo CAPITAL_ALBANIA;
+    echo CAPITAL_MACEDONIA; 
+    ?>  
+</body>
+</html>
